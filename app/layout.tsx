61b388f7c227f84aebc8cd552a8023fc6e48f0ec
Anjula <anjulanimesh9@gmail.com>
2026-08-27 04:40:54 +0200

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,12 +91,56 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
 
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {
+              if(f.fbq)return;
+              n=f.fbq=function(){
+                n.callMethod
+                  ? n.callMethod.apply(n,arguments)
+                  : n.queue.push(arguments)
+              };
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(
+              window,
+              document,
+              'script',
+              'https://connect.facebook.net/en_US/fbevents.js'
+            );
+
+            fbq('init', '1682495002854308');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1682495002854308&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+        {/* Google AdSense */}
         <Script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5400699100789727"
-  crossOrigin="anonymous"
-  strategy="afterInteractive"
-/>
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5400699100789727"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
